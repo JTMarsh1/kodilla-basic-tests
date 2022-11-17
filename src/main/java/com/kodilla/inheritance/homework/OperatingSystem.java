0ddb2a0f85b6a62b1f5f0590e25106fa9152893e
Lorenco Waleka <lorenco.waleka96@gmail.com>
2022-11-17 19:54:48 +0100

    new2

--- src/main/java/com/kodilla/inheritance/homework/OperatingSystem.java
+++ src/main/java/com/kodilla/inheritance/homework/OperatingSystem.java
@@ -3,12 +3,14 @@ package com.kodilla.inheritance.homework;
 public class OperatingSystem {
     private int year;
 
-    public OperatingSystem(int year){
+    public OperatingSystem(int year) {
         this.year = year;
     }
-    public void turnOn(){
+
+    public void turnOn() {
         System.out.println("Welcome to the system ");
     }
+
     public void turnOff() {
         System.out.println("The system is shutting down");
 
