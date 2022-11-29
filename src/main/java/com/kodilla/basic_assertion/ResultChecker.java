a3562da789d0b2abd8457ebc199ae0026ae806d2
Lorenco Waleka <lorenco.waleka96@gmail.com>
2022-11-29 18:59:48 +0100

    Zd.3.5 modyfikacja

--- src/main/java/com/kodilla/basic_assertion/ResultChecker.java
+++ src/main/java/com/kodilla/basic_assertion/ResultChecker.java
@@ -1,7 +1,7 @@
 package com.kodilla.basic_assertion;
 
 public class ResultChecker {
-    public static boolean assertEquals(int expected, int actual) {
+    public static boolean assertEquals(double expected, int actual) {
         return expected == actual;
     }
 
