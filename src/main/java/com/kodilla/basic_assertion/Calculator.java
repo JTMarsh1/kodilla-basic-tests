bfcc1c24e9245b668ff92b6039442b627205c7dc
Lorenco Waleka <lorenco.waleka96@gmail.com>
2022-12-03 20:47:13 +0100

    Zd.3.6 modyfikacja

--- src/main/java/com/kodilla/basic_assertion/Calculator.java
+++ src/main/java/com/kodilla/basic_assertion/Calculator.java
@@ -1,6 +1,9 @@
 package com.kodilla.basic_assertion;
 
 public class Calculator {
+    private double delta;
+    private double[] values;
+
     public int sum(int a, int b) {
         return a + b;
     }
@@ -13,4 +16,15 @@ public class Calculator {
         return Math.pow(a, b);
     }
 
+    public double getAverage() {
+        if (this.values.length == 0) {
+            return 0;
+        }
+        double sum = 0;
+        for (int i = 0; i < this.values.length; i++) {
+            sum += this.values[i];
+        }
+        return sum / this.values.length;
+    }
 }
+
